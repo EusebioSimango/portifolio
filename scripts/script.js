6937c16bcd97777e0da061d591750cc1b4416ab3
jeffer <jeffersunde72@gmail.com>
2022-03-22 09:25:31 +0200

    New functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,34 @@
-const years = document.querySelector("span")
+const button = document.querySelector("button")
+const header = document.querySelector("header.show")
+const main = document.querySelector("main.show")
+const footer = document.querySelector("footer.show")
+const message = document.querySelector("div.was-clicked")
+const seconds = document.querySelector("div.was-clicked span")
 
-const func = () => { 
-    
-        years.innerText = Number(years.innerText) + 1
-        console.log(years.innerText)
-    
-}
+button.addEventListener("click", () => {
+        header.classList.remove("show")
+        main.classList.remove("show")
+        footer.classList.remove("show")
+        button.classList.remove("show")
+        message.classList.add("show")
 
-func()
+        setTimeout(() => {
+                seconds.innerText -= 1
+        }, 1000)
+
+        setTimeout(() => {
+                seconds.innerText -= 1
+        }, 2000)
+        
+        setTimeout(() => {
+                seconds.innerText -= 1
+        }, 3000)
+
+        setTimeout(() => {
+                seconds.innerText -= 1
+        }, 4000)
+
+        setTimeout(() => {
+                seconds.innerText -= 1
+        }, 5000)
+})
