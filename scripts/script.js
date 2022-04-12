391dc20a5c87952ffaf4e326224bc625e49c642b
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-04-12 21:32:00 +0200

    Some animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,20 @@
-const button = document.querySelector("button")
-const header = document.querySelector("header.show")
-const main = document.querySelector("main.show")
-const footer = document.querySelector("footer.show")
-const message = document.querySelector("div.was-clicked")
-const seconds = document.querySelector("div.was-clicked span")
+const sr = ScrollReveal({
+        origin: 'top',
+        distance: '50px',
+        duration: 2500,
+        reset: true
+    });
+    
+    ScrollReveal().reveal('header', { delay: 100 });
+    ScrollReveal().reveal('section .text', { delay: 200 });
+    ScrollReveal().reveal('section img', { delay: 200 });
+    
+    ScrollReveal().reveal('section .group', { delay: 500 });
+    ScrollReveal().reveal('section img', { delay: 300 });
+    
+    ScrollReveal().reveal('section .project', { delay: 400 });
+    ScrollReveal().reveal('section .service', { delay: 400 });
 
-button.addEventListener("click", () => {
-        header.classList.remove("show")
-        main.classList.remove("show")
-        footer.classList.remove("show")
-        button.classList.remove("show")
-        message.classList.add("show")
-
-        setTimeout(() => {
-                seconds.innerText -= 1
-        }, 1000)
-
-        setTimeout(() => {
-                seconds.innerText -= 1
-        }, 2000)
-        
-        setTimeout(() => {
-                seconds.innerText -= 1
-        }, 3000)
-
-        setTimeout(() => {
-                seconds.innerText -= 1
-        }, 4000)
-
-        setTimeout(() => {
-                seconds.innerText -= 1
-        }, 5000)
-})
+    ScrollReveal().reveal('section .text h1', { delay: 400 });
+    
+    ScrollReveal().reveal('section .container', { delay: 600 });
