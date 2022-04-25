53f4f5a02a142da297e0453103815aa355d968ff
JefferMarcelino <jeffersunde72@gmail.com>
2022-04-25 20:14:24 +0200

    Adding my logo and favicon, and removing an animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,3 @@
-const sections = document.querySelectorAll("section")
-
-document.addEventListener("scroll", () => {
-      console.log("scrolling")
-      
-      const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
-      console.log(checkpoint)
-
-      sections.forEach(section => {
-
-            const divTop = section.offsetTop
-            const divHeight = section.offsetHeight
-
-
-            const checkpointStart = checkpoint >= divTop
-            const checkpointEnd = checkpoint <= divTop + divHeight
-
-
-        if (checkpointStart && checkpointEnd) {
-                section.classList.add("scroll")
-      } else {
-              section.classList.remove("scroll")
-        }
-      })
-})
-
 const links = document.querySelectorAll("header nav ul li a")
 
 links.forEach(link => {
