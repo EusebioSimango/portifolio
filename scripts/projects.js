cb3ee8f8a34c9cb7a61d691a0ef7ef12ec7e06f7
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-30 03:26:23 +0200

    end with the array of projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -14,6 +14,27 @@ const projects = [
 		description: 'A landing-page for a hair salon, where you can find a lot informations with just a click.',
 		gitRepo: 'https://github.com/JefferMarcelino/beautysalon-nlw6-origin',
 		techs: 'HTML, CSS and JavaScript',
+	},
+	{
+		url: 'https://jeffermarcelino.github.io/advice/',
+		title: 'Advice',
+		description: 'Simple page which returns you a random advice that might improve your situation. I recommend!!!',
+		gitRepo: 'https://github.com/JefferMarcelino/advice',
+		techs: 'HTML, CSS, JavaScript and Advice Slip JSON API',
+	},
+	{
+		url: '',
+		title: 'Rocket.Q',
+		description: 'This is a web application where you can make anonymous questions on a room and the room administrator can see and answer or delete the question.',
+		gitRepo: 'https://github.com/JefferMarcelino/rocket.q-nlw6-discover',
+		techs: 'HTML, CSS, JavaScript, ExpressJS and SQLite3',
+	},
+	{
+		url: '',
+		title: 'Codelandia Challenges',
+		description: 'This was created by me, here you find the solutions of the Codelandia server challenges on Discord. And you can find others simple front-end projects.',
+		gitRepo: 'https://github.com/JefferMarcelino/codelandia-challenges',
+		techs: 'HTML, CSS and JavaScript',
 	}
 ]
 
